a3d643fcdf5df5b9c3b1b5ca5909fe080e37349a
Anna Kompan <annakompanch@gmail.com>
2025-08-13 17:26:57 +0300

    upd

--- Fibonacci.java
+++ Fibonacci.java
@@ -14,15 +14,6 @@ public class Fibonacci {
         if (n <= 1){
             return n;
         }
-
-        int a = 0;
-        int b = 1;
-
-        for (int i = 2; i < n + 1; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
-        }
-        return b;
+        return calcFibonacci(n - 1) + calcFibonacci(n - 2);
     }
 }
